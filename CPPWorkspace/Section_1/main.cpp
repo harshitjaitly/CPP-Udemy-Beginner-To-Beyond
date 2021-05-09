b712e079db754bf8c10d9720f3170a131282aaeb
harshitjaitly <harshitjaitly@gmail.com>
2021-05-09 10:53:24 +0530

    Section 1 complete

---
+++ CPPWorkspace/Section_1/main.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+int main()
+{
+	int fav_num ;
+	std::cout << "Enter your FAVORITE number between 1 and 100 : " ;
+	std::cin >> fav_num ;
+	std::cout << "Amazing!! That's my favorite number too!\nNo really!!, "<< fav_num <<" is my favorite number" << std::endl;
+	return 0;
+}
